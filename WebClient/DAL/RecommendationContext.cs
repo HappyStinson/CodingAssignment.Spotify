f21ac4d02975114aaff5709a36238012a7de21f2
happy <happy@DESKTOP-VN3AUSI>
2017-03-01 21:39:39 +0100

    Add model, context and update packages

---
+++ WebClient/DAL/RecommendationContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using WebClient.Models;
+
+namespace WebClient.DAL
+{
+    public class RecommendationContext : DbContext
+    {
+        public RecommendationContext() : base("​DefaultConnection ") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new RecommendationContextInitializer());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<RecommendationBasis> Basis { get; set; }
+    }
+}
---
+++ WebClient/DAL/RecommendationContextInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using WebClient.Models;
+
+namespace WebClient.DAL
+{
+    internal sealed class RecommendationContextInitializer : DropCreateDatabaseAlways<RecommendationContext>
+    {
+        protected override void Seed(RecommendationContext context)
+        {
+            context.Basis.Add(new RecommendationBasis {Artist = "Kiss"});
+        }
+    }
+}
