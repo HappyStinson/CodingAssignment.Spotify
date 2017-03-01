5812f81a907b184bbb0cfff69b0372f839198be5
happy <happy@DESKTOP-VN3AUSI>
2017-03-01 20:59:42 +0100

    Add SpotyfiApiClient to HomeController

--- WebClient/Controllers/HomeController.cs
+++ WebClient/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
+using CodingAssignment.Spotify.ApiClient;
 
 namespace WebClient.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            var client = new SpotifyApiClient();
+
+            var response = await client.SearchArtistsAsync("kissing candice");
+            ViewBag.Response = response;
+            var artists = response.Artists;
+            var artist = artists.Items[0];
+
+            ViewBag.Message = $"{artist.Name}, popularity: {artist.Popularity}";
+
+            if (artist.Images.Count != 0)
+                ViewBag.ImageUrl = artist.Images[0].Url;
+
             return View();
         }
 
