38b4da1e0acc7bd191dea87ec13b6f731e2838d9
happy <happy@DESKTOP-VN3AUSI>
2017-03-01 23:44:29 +0100

    Remove SpotifyApiClient from HomeController

--- WebClient/Controllers/HomeController.cs
+++ WebClient/Controllers/HomeController.cs
@@ -1,25 +1,11 @@
-﻿using System.Threading.Tasks;
-using System.Web.Mvc;
-using CodingAssignment.Spotify.ApiClient;
+﻿using System.Web.Mvc;
 
 namespace WebClient.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
-            var client = new SpotifyApiClient();
-
-            var response = await client.SearchArtistsAsync("kissing candice");
-            ViewBag.Response = response;
-            var artists = response.Artists;
-            var artist = artists.Items[0];
-
-            ViewBag.Message = $"{artist.Name}, popularity: {artist.Popularity}";
-
-            if (artist.Images.Count != 0)
-                ViewBag.ImageUrl = artist.Images[0].Url;
-
             return View();
         }
 
